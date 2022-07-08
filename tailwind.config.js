6159ae7438ba9b2077258cb28978912197c2d6a7
jam54 <git@jam54.com>
2022-07-08 13:53:21 +0200

    Fixed a bug where dark mode was disabled on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./*.{html,js,ts,jsx,tsx}",
-      "./js/*.{html,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-        colors: {
-            'navbar-blue': '#2196f3',
-            'zelf-background-color': '#282a2e',
-            'lightgrey-blue': '#4e86b0',
-        },
-        backgroundSize: {
-            'size-200': '200% 200%',
-        },
-        backgroundPosition: {
-            'pos-0': '0% 0%',
-            'pos-100': '100% 100%',
+    content: [
+        "./*.{html,js,ts,jsx,tsx}",
+        "./js/*.{html,js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'navbar-blue': '#2196f3',
+                'zelf-background-color': '#282a2e',
+                'lightgrey-blue': '#4e86b0',
+            },
+            backgroundSize: {
+                'size-200': '200% 200%',
+            },
+            backgroundPosition: {
+                'pos-0': '0% 0%',
+                'pos-100': '100% 100%',
+            },
         },
     },
-  },
-  plugins: [],
+    plugins: [],
+    darkMode: 'class',
 }

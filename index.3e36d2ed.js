5f9928521da045797a2611a2742e961451199ba8
jam54 <git@jam54.com>
2022-12-29 14:39:37 +0100

    Published version 1.0.4

--- index.3e36d2ed.js
+++ index.3e36d2ed.js
@@ -1,2 +1,2 @@
 document.getElementById("hamburger").addEventListener("click",(function(e){const n=document.getElementById("mobile-menu-4");n.classList.contains("hidden")?n.classList.remove("hidden"):n.classList.add("hidden")}));
-//# sourceMappingURL=about.d1cd1f29.js.map
+//# sourceMappingURL=index.3e36d2ed.js.map

e16ef16b24ede639d75f82495deaa195b2706f82
jam53 <deniz.callens@outlook.com>
2023-06-15 11:06:03 +0200

    Published version 1.0.11

--- index.3e36d2ed.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("hamburger").addEventListener("click",(function(e){const n=document.getElementById("mobile-menu-4");n.classList.contains("hidden")?n.classList.remove("hidden"):n.classList.add("hidden")}));
-//# sourceMappingURL=index.3e36d2ed.js.map

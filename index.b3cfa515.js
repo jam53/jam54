0edd75009575bf55c88c3af45115ba83b039d4d7
jam54 <git@jam54.com>
2023-06-15 11:06:03 +0200

    Published version 1.0.11

---
+++ index.b3cfa515.js
@@ -0,0 +1,2 @@
+document.getElementById("hamburger").addEventListener("click",function(e){var d=document.getElementById("mobile-menu-4");d.classList.contains("hidden")?d.classList.remove("hidden"):d.classList.add("hidden")});
+//# sourceMappingURL=index.b3cfa515.js.map
---
+++ index.bfffef24.js
@@ -0,0 +1,2 @@
+document.getElementById("hamburger").addEventListener("click",function(e){let d=document.getElementById("mobile-menu-4");d.classList.contains("hidden")?d.classList.remove("hidden"):d.classList.add("hidden")});
+//# sourceMappingURL=index.bfffef24.js.map

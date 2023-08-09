0fa6acc1f0a53c12c47e05bd245613db2aae2823
jam53 <deniz.callens@outlook.com>
2023-08-09 15:09:40 +0200

    Published version 1.0.12

--- index.bfffef24.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("hamburger").addEventListener("click",function(e){let d=document.getElementById("mobile-menu-4");d.classList.contains("hidden")?d.classList.remove("hidden"):d.classList.add("hidden")});
-//# sourceMappingURL=index.bfffef24.js.map

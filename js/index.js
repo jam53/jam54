ddae0260c58b05a8fc1d53e8a7a8a52fad21dc67
jam54 <git@jam54.com>
2024-02-22 14:26:07 +0100

    Added a fade in animation to most of the elements

--- js/index.js
+++ js/index.js
@@ -8,3 +8,32 @@ document.getElementById("hamburger").addEventListener("click", function (event)
 
     mobileMenu.classList.contains("hidden") ? mobileMenu.classList.remove("hidden") : mobileMenu.classList.add("hidden");
 });
+
+/**
+ * This handles the fade in animation of elements
+ */
+const animatedElements = document.querySelectorAll('.animated-element');
+
+const observer = new IntersectionObserver((entries) => {
+    let delay = 0;
+    entries.forEach((entry) => {
+        if (entry.isIntersecting)
+        {
+            if (entry.intersectionRatio === 1)
+            {//Check if the element is fully visible, i.e. when the element is immediately visible on page load
+                entry.target.classList.add('quick');
+            }
+
+            setTimeout(() => {
+                entry.target.classList.add('in-view');
+            }, delay);
+            delay += 75; //Increment delay for subsequent elements to introduce a slight delay when multiple elements are revealed simultaneously, e.g. during initial page load
+
+            observer.unobserve(entry.target); //Unobserve the element after it's in view to prevent further animations
+        }
+    });
+}, { threshold: 0.3 }); //How much percent of the element has to be visible, before the fade in animation is played
+
+animatedElements.forEach(element => {
+    observer.observe(element);
+});

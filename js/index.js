83f18301537389ef6f9420274b320b87707baf29
jam53 <47756749+jam53@users.noreply.github.com>
2024-03-15 16:34:06 +0100

    Fix hover animations not working on elements with fade-in animation

--- js/index.js
+++ js/index.js
@@ -26,6 +26,14 @@ const observer = new IntersectionObserver((entries) => {
 
             setTimeout(() => {
                 entry.target.classList.add('in-view');
+
+                //If we don't remove these CSS classes that are used to do the fade in effect, hover animations and other animations on this element will no longer behave as expected since these CSS classes override some properties like `transition`
+                setTimeout(() => {
+                    entry.target.classList.remove('animated-element');
+                    entry.target.classList.remove('quick');
+                    entry.target.classList.remove('in-view');
+                }, parseFloat(window.getComputedStyle(entry.target).transitionDuration) * 1000) //Wait for the fade-in effect to finish before removing the CSS classes
+
             }, delay);
             delay += 75; //Increment delay for subsequent elements to introduce a slight delay when multiple elements are revealed simultaneously, e.g. during initial page load
 

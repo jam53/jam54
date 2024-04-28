d0f5ea766c48944db55b55c41169d613bdc01801
jam54 <git@jam54.com>
2024-04-28 08:34:20 +0200

    Create a custom web element for the top navbar to avoid code duplication

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: [
         "./*.{html,js,ts,jsx,tsx}",
-        "./js/*.{html,js,ts,jsx,tsx}"
+        "./js/*/*.{html,js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {

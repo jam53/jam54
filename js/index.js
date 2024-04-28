bf8e93394b790615cfa95e063f4d11bf30b302f2
jam54 <git@jam54.com>
2024-04-28 08:34:20 +0200

    Create a custom web element for the top navbar to avoid code duplication

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 /**
- * When the user is on mobile, a hamburger icon appears.
- * This function will unfold/fold the menu options when the user clicks on the hamburger.
- */
-document.getElementById("hamburger").addEventListener("click", function (event)
-{
-    const mobileMenu = document.getElementById("mobile-menu-4");
-
-    mobileMenu.classList.contains("hidden") ? mobileMenu.classList.remove("hidden") : mobileMenu.classList.add("hidden");
-});
-
-/**
  * This handles the fade in animation of elements
  */
 const animatedElements = document.querySelectorAll('.animated-element');

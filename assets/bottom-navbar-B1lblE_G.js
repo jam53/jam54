0a6a7fe2f3aa54aede5ba300158772e2fda9fffe
jam54 <git@jam54.com>
2025-06-13 21:31:28 +0200

    chore(release): version 1.4.0

--- assets/bottom-navbar-B1lblE_G.js
+++ assets/bottom-navbar-B1lblE_G.js
@@ -49,7 +49,7 @@
                         <a href="https://ko-fi.com/jam54" class="text-gray-500 hover:text-gray-900 dark:hover:text-white transition-all">
                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734 4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09-.443-.441-3.368-3.049-4.034-3.954-.709-.965-1.041-2.7-.091-3.71.951-1.01 3.005-1.086 4.363.407 0 0 1.565-1.782 3.468-.963 1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z"/></svg>
                         </a>
-                        <a href="mailto:info@jam54.com" target="_self" class="text-gray-500 hover:text-gray-900 dark:hover:text-white transition-all">
+                        <a href="mailto:website@jam54.com" target="_self" class="text-gray-500 hover:text-gray-900 dark:hover:text-white transition-all">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 -1.5 18 18" aria-hidden="true"><path fill-rule="evenodd" d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" clip-rule="evenodd" /></svg>
                         </a>
                     </div>

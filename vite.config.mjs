d6fc1a405ed7f821648d1849a6c6c11adcb3eb4d
jam54 <git@jam54.com>
2026-06-22 16:53:02 +0200

    fix: assets referenced in injected tags missing in build
    Assets referenced in the Vite plugins which inject tags in head/body across all pages, ended up not being included during build.
    
    This was due to the fact that these plugins injected the tags *after* Vite had already scanned the HTML files for dependencies. This change ensure that the tags are injected before Vite scans for dependencies. Thus making it so the referenced assets are included in the build.

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,6 @@ export default defineConfig({
                 programs: './programs.html',
                 smashandflypatchnotes: './smashandfly-patchnotes.html',
                 stelexopatchnotes: './stelexo-patchnotes.html',
-                maincss: "./css/main.css",
             }
         }
     },
@@ -31,38 +30,44 @@ export default defineConfig({
 function injectTagsIntoHead() {
     return {
         name: "inject-into-head",
-        transformIndexHtml() {
-            return {
-                tags: [
-                    { tag: "meta", attrs: { charset: "utf-8" }, injectTo: "head" },
-                    { tag: "meta", attrs: { name: "description", content: "Software development and game development by jam54." }, injectTo: "head" },
-                    { tag: "meta", attrs: { name: "viewport", content: "width=device-width, initial-scale=1" }, injectTo: "head" },
-                    { tag: "meta", attrs: { name: "theme-color", content: "#fafafa" }, injectTo: "head" },
+        transformIndexHtml: {
+            // Ensures that the tags are injected before Vite runs its HTML parser which scans for dependencies.
+            // Should we omit this, the assets referenced in these tags wouldn't be included when Vite builds the project,
+            // since these tags wouldn't be present yet in the HTML when Vite scans for dependencies.
+            order: "pre",
+            handler() {
+                return {
+                    tags: [
+                        { tag: "meta", attrs: { charset: "utf-8" }, injectTo: "head" },
+                        { tag: "meta", attrs: { name: "description", content: "Software development and game development by jam54." }, injectTo: "head" },
+                        { tag: "meta", attrs: { name: "viewport", content: "width=device-width, initial-scale=1" }, injectTo: "head" },
+                        { tag: "meta", attrs: { name: "theme-color", content: "#fafafa" }, injectTo: "head" },
 
-                    { tag: "meta", attrs: { property: "og:title", content: "Jam54" }, injectTo: "head" },
-                    { tag: "meta", attrs: { property: "og:type", content: "website" }, injectTo: "head" },
-                    { tag: "meta", attrs: { property: "og:url", content: "jam54.com" }, injectTo: "head" },
-                    { tag: "meta", attrs: { property: "og:image", content: "./icon.png" }, injectTo: "head" },
+                        { tag: "meta", attrs: { property: "og:title", content: "Jam54" }, injectTo: "head" },
+                        { tag: "meta", attrs: { property: "og:type", content: "website" }, injectTo: "head" },
+                        { tag: "meta", attrs: { property: "og:url", content: "jam54.com" }, injectTo: "head" },
+                        { tag: "meta", attrs: { property: "og:image", content: "./icon.png" }, injectTo: "head" },
 
-                    { tag: "link", attrs: { rel: "manifest", href: "site.webmanifest" }, injectTo: "head" },
-                    { tag: "link", attrs: { rel: "apple-touch-icon", href: "./icon.png" }, injectTo: "head" },
-                    { tag: "link", attrs: { rel: "icon", href: "favicon.ico", type: "image/x-icon" }, injectTo: "head" },
-                    { tag: "link", attrs: { rel: "shortcut icon", href: "favicon.ico", type: "image/x-icon" }, injectTo: "head" },
+                        { tag: "link", attrs: { rel: "manifest", href: "site.webmanifest" }, injectTo: "head" },
+                        { tag: "link", attrs: { rel: "apple-touch-icon", href: "./icon.png" }, injectTo: "head" },
+                        { tag: "link", attrs: { rel: "icon", href: "favicon.ico", type: "image/x-icon" }, injectTo: "head" },
+                        { tag: "link", attrs: { rel: "shortcut icon", href: "favicon.ico", type: "image/x-icon" }, injectTo: "head" },
 
-                    { tag: "link", attrs: { rel: "stylesheet", href: "css/main.css" }, injectTo: "head" },
-                    {
-                        tag: "script",
-                        attrs: {
-                            defer: true,
-                            src: "https://jam54-api.jam54.com/main.js",
-                            "data-host-url": "https://jam54-api.jam54.com/",
-                            "data-website-id": "07f62870-33ef-4ac3-b2e6-fd531df84517",
-                            "data-domains": "jam54.com,www.jam54.com",
+                        { tag: "link", attrs: { rel: "stylesheet", href: "css/main.css" }, injectTo: "head" },
+                        {
+                            tag: "script",
+                            attrs: {
+                                defer: true,
+                                src: "https://jam54-api.jam54.com/main.js",
+                                "data-host-url": "https://jam54-api.jam54.com/",
+                                "data-website-id": "07f62870-33ef-4ac3-b2e6-fd531df84517",
+                                "data-domains": "jam54.com,www.jam54.com",
+                            },
+                            injectTo: "head",
                         },
-                        injectTo: "head",
-                    },
-                ]
-            };
+                    ]
+                };
+            }
         }
     }
 }
@@ -73,14 +78,20 @@ function injectTagsIntoHead() {
 function injectTagsIntoBody() {
     return {
         name: "inject-into-body",
-        transformIndexHtml() {
-            return {
-                tags: [
-                    { tag: "script", attrs: { type: "module", src: "js/index.js" }, injectTo: "body" },
-                    { tag: "script", attrs: { type: "module", src: "js/customElements/top-navbar.js" }, injectTo: "body" },
-                    { tag: "script", attrs: { type: "module", src: "js/customElements/bottom-navbar.js" }, injectTo: "body" },
-                ]
-            };
+        transformIndexHtml: {
+            // Ensures that the tags are injected before Vite runs its HTML parser which scans for dependencies.
+            // Should we omit this, the assets referenced in these tags wouldn't be included when Vite builds the project,
+            // since these tags wouldn't be present yet in the HTML when Vite scans for dependencies.
+            order: "pre",
+            handler() {
+                return {
+                    tags: [
+                        { tag: "script", attrs: { type: "module", src: "js/index.js" }, injectTo: "body" },
+                        { tag: "script", attrs: { type: "module", src: "js/customElements/top-navbar.js" }, injectTo: "body" },
+                        { tag: "script", attrs: { type: "module", src: "js/customElements/bottom-navbar.js" }, injectTo: "body" },
+                    ]
+                };
+            }
         }
     }
 }
